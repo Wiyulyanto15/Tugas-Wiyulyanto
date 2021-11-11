23100976cd129b4aa97dad0dc74c8ace0295bea5
Wiyulyanto15 <94118476+Wiyulyanto15@users.noreply.github.com>
2021-11-11 20:10:04 +0700

    Wiyulyanto FlaskProject

--- hello.py
+++ hello.py
@@ -2,5 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def Hello_Sirah():
-    return 'Hello, Wiyulyanto! Ini pertama kali saya melakukannya '+__name__
+def hello_future():
+	return 'Hello, Future!'
+	
